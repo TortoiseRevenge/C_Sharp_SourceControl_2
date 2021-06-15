cb18eb6f063b97a0ccdd58c389650b6819c1658a
Anna <22huanga@elmbrookstudents.org>
2021-06-15 14:42:34 -0500

    fifth

--- C_Sharp_SourceControl_2/C_Sharp_SourceControl_2/Program.cs
+++ C_Sharp_SourceControl_2/C_Sharp_SourceControl_2/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace C_Sharp_SourceControl_2
 {
     class Program
@@ -7,6 +7,13 @@ namespace C_Sharp_SourceControl_2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+        public void Fifth(List<int> num) {
+            List<int> fives = new List<int>();
+            for (int x=0; x<fives.Count; x+=5) {
+                    fives.Add(num[x]);
+            }
+            Console.WriteLine(fives);
         }
     }
 }
