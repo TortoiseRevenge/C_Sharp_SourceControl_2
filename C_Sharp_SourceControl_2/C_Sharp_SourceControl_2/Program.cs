8434ed780975cdc5a24ac3ee66bcc2a507caacef
Anna <22huanga@elmbrookstudents.org>
2021-06-15 14:25:57 -0500

    sum

--- C_Sharp_SourceControl_2/C_Sharp_SourceControl_2/Program.cs
+++ C_Sharp_SourceControl_2/C_Sharp_SourceControl_2/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace C_Sharp_SourceControl_2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+        }
+
+        public void displaySum(List<int> nums)
+        {
+            int sum = 0;
+            foreach (int x in nums)
+            {
+                sum += x;
+            }
+            Console.WriteLine(sum);
         }
     }
 }
