6bd332aeb55f040adb24ed9aa97f7d5a769fcb2e
Joel Kuriakose <1joel.kuriakose@gmail.com>
2021-06-15 14:18:26 -0500

    DivisibleByThree

--- C_Sharp_SourceControl_2/C_Sharp_SourceControl_2/Program.cs
+++ C_Sharp_SourceControl_2/C_Sharp_SourceControl_2/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace C_Sharp_SourceControl_2
 {
     class Program
@@ -7,6 +7,20 @@ namespace C_Sharp_SourceControl_2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public static void divisibleByThree(List<int> num)
+        {
+            Console.WriteLine("Numbers Divisible by Three: ");
+            foreach (int number in num)
+            {
+   
+                if(number != 0 && number%3 == 0)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+          
         }
     }
 }
